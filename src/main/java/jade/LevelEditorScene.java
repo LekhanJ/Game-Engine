e14c57d5e4566a43a57d3484461fa5c54bc4e790
LekhanJ <jomiwalelekhan@gmail.com>
2024-11-13 17:05:46 +0530

    Window creation complete

---
+++ src/main/java/jade/LevelEditorScene.java
@@ -0,0 +1,11 @@
+package jade;
+
+public class LevelEditorScene extends Scene {
+    public LevelEditorScene() {
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
---
+++ src/main/java/jade/Window.java
@@ -0,0 +1,129 @@
+package jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import util.Time;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11C.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private static Window window = null;
+
+    private static Scene currentScene;
+
+    private Window() {
+        this.width = 1280;
+        this.height = 720;
+        this.title = "Engine Window";
+    }
+
+    public static void changeScene(int newScene) {
+        switch (newScene) {
+            case 0:
+                currentScene = new LevelEditorScene();
+                // currentScene.init()
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                break;
+            default:
+                assert false : "Unknown scene '" + newScene + "'";
+                break;
+        }
+    }
+
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+
+        // Free the memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void init() {
+        // Error callback setup
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW.");
+        }
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_FALSE);
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL); // returns a long which represents the memory location where out window will be stored.
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window.");
+        }
+
+        // Setting callbacks
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
+        // Making OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
+    }
+
+    private void loop() {
+        GL.createCapabilities();
+
+        float beginTime = Time.getTime();
+        float endTime = Time.getTime();
+
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            glfwPollEvents();
+
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+                System.out.println("Space key is pressed");
+            }
+
+            if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_1)) {
+                System.out.println("Left click is pressed");
+            }
+
+            glfwSwapBuffers(glfwWindow);
+
+            endTime = Time.getTime();
+            float dt = endTime - beginTime;
+            beginTime = endTime;
+        }
+    }
+}
