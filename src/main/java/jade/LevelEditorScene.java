2a4e3e103b9bc1327375966612b6c8fe16f2ac15
LekhanJ <jomiwalelekhan@gmail.com>
2024-11-22 20:50:33 +0530

    Sprite sheet support added

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -1,6 +1,8 @@
 package jade;
 
+import components.Sprite;
 import components.SpriteRenderer;
+import components.SpriteSheet;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
@@ -17,24 +19,30 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init() {
-        this.gameObjects = new ArrayList<>();
+        loadResources();
+
         this.camera = new Camera(new Vector3f());
+        this.gameObjects = new ArrayList<>();
         this.renderer = new Renderer();
 
+        SpriteSheet sprites = AssetPool.getSpriteSheet("assets/images/MarioSS.png");
+
         GameObject mario = new GameObject("Mario", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
-        mario.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/mario.png")));
+        mario.addComponent(new SpriteRenderer(sprites.getSprite(2)));
         this.addGameObjectToScene(mario);
 
         GameObject goomba = new GameObject("Goomba", new Transform(new Vector2f(400, 100), new Vector2f(128, 128)));
-        goomba.addComponent(new SpriteRenderer(AssetPool.getTexture("assets/images/goomba.png")));
+        goomba.addComponent(new SpriteRenderer(sprites.getSprite(5)));
         this.addGameObjectToScene(goomba);
-
-        loadResources();
     }
 
     // Makes sure that we are creating the shader, compiling and linking it as well.
     private void loadResources() {
         AssetPool.getShader("assets/shaders/default.glsl");
+        AssetPool.addSpriteSheet(
+                "assets/images/MarioSS.png",
+                new SpriteSheet(AssetPool.getTexture("assets/images/MarioSS.png"), 17, 16, 14, 13)
+        );
     }
 
     @Override
--- src/main/java/renderer/Texture.java
+++ src/main/java/renderer/Texture.java
@@ -34,6 +34,7 @@ public class Texture {
 
     private String filepath;
     private int texID;
+    private int width, height;
 
     public Texture(String filepath) {
         this.filepath = filepath;
@@ -58,6 +59,8 @@ public class Texture {
         ByteBuffer image = stbi_load(filepath, width, height, channels, 0);
 
         if (image != null) {
+            this.height = height.get(0);
+            this.width = width.get(0);
             if (channels.get(0) == 3) {
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width.get(0), height.get(0), 0, GL_RGB, GL_UNSIGNED_BYTE, image);
             } else if (channels.get(0) == 4) {
@@ -78,5 +81,13 @@ public class Texture {
 
     public void unbind() {
         glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
     }
 }
